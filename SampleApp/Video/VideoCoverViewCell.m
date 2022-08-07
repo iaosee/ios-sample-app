b123d00ce10734a288bdfa26804ae805d9fef00e
iaosee <iaosee@outlook.com>
2022-08-07 17:51:22 +0800

    feat: create single VideoPlayer

--- SampleApp/Video/VideoCoverViewCell.m
+++ SampleApp/Video/VideoCoverViewCell.m
@@ -6,13 +6,9 @@
 //
 
 #import "VideoCoverViewCell.h"
-#import <AVFoundation/AVFoundation.h>
+#import "VideoPlayer.h"
 
 @interface VideoCoverViewCell()
-
-@property(nonatomic, strong, readwrite) AVPlayer *player;
-@property(nonatomic, strong, readwrite) AVPlayerItem *playerItem;
-@property(nonatomic, strong, readwrite) AVPlayerLayer *playerLayer;
 
 @property(nonatomic, strong, readwrite) UIImageView *coverView;
 @property(nonatomic, strong, readwrite) UIImageView *playButton;
@@ -37,78 +33,26 @@
             _playButton;
         })];
         
-        UIGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(_tapPlayButtonHandler)];
+        UIGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(_tapToPlayHandler)];
         [self addGestureRecognizer:tapGesture];
         
-        [[NSNotificationCenter defaultCenter] addObserver:self
-                                                 selector:@selector(_playEndHandler)
-                                                     name:AVPlayerItemDidPlayToEndTimeNotification
-                                                   object:nil];
     }
     
     return self;
 }
 
 - (void)dealloc {
-    [[NSNotificationCenter defaultCenter] removeObserver:self];
-    [_playerItem removeObserver:self forKeyPath:@"status" context:nil];
-    [_playerItem removeObserver:self forKeyPath:@"loadedTimeRanges" context:nil];
+    
 }
 
 -(void) layoutWithVideoCoverUrl: (NSString *) videoCoverUrl videoUrl:(NSString *)videoUrl {
     _coverView.image = [UIImage imageNamed:videoCoverUrl];
     _playButton.image = [UIImage imageNamed:@"icon.bundle/videoPlay@3x.png"];
     _videoUrl = videoUrl;
-    
 }
 
-- (void) _tapPlayButtonHandler {
-    NSLog(@"_tapPlayButtonHandler, %@", _videoUrl);
-    
-    NSURL *url = [NSURL URLWithString:_videoUrl];
-    AVAsset *asset = [AVAsset assetWithURL:url];
-    
-    _playerItem = [AVPlayerItem playerItemWithAsset:asset];
-    _player = [AVPlayer playerWithPlayerItem:_playerItem];
-    _playerLayer = [AVPlayerLayer playerLayerWithPlayer:_player];
-
-    [_playerItem addObserver:self forKeyPath:@"status" options:NSKeyValueObservingOptionNew context:nil];
-    [_playerItem addObserver:self forKeyPath:@"loadedTimeRanges" options:NSKeyValueObservingOptionNew context:nil];
-    _playerLayer.frame = _coverView.bounds;
-    [_coverView.layer addSublayer:_playerLayer];
-    
-    CMTime duration = _playerItem.duration;
-    CGFloat videoDuration = CMTimeGetSeconds(duration);
-    NSLog(@"videoDuration - %f", videoDuration);
-
-    [_player addPeriodicTimeObserverForInterval:CMTimeMake(1, 1) queue:dispatch_get_main_queue() usingBlock:^(CMTime time) {
-        NSLog(@"播放进度 - %f", CMTimeGetSeconds(time));
-    }];
-    
-//    [_player play];
+- (void) _tapToPlayHandler {
+    VideoPlayer *player = [VideoPlayer Player];
+    [player playVideoWithUrl:_videoUrl attachView:_coverView];
 }
-
-- (void) _playEndHandler {
-    [_playerLayer removeFromSuperlayer];
-    _playerItem = nil;
-    _player = nil;
-    
-    [_player seekToTime:CMTimeMake(0, 1)];
-    [_player play];
-}
-
-#pragma mark KVO
-
-- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context {
-    if ([keyPath isEqualToString:@"status"]) {
-        if(((NSNumber *)[change objectForKey:NSKeyValueChangeNewKey]).integerValue == AVPlayerStatusReadyToPlay) {
-            [_player play];
-        } else {
-            NSLog(@"AVPlayerStatus - %@", change);
-        }
-    } else if ([keyPath isEqualToString:@"loadedTimeRanges"]) {
-        NSLog(@"缓冲 - %@", [change objectForKey:NSKeyValueChangeNewKey]);
-    }
-}
-
 @end
