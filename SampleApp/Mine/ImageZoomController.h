33ba5dece39d29342b5606740f8c7b94024485f7
iaosee <iaosee@outlook.com>
2022-08-20 01:41:42 +0800

    feat: add scroll view controller with storyboard

---
+++ SampleApp/Mine/ImageZoomController.h
@@ -0,0 +1,16 @@
+//
+//  ImageZoomController.h
+//  SampleApp
+//
+//  Created by xiaofeng on 2022/8/20.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ImageZoomController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SampleApp/Mine/ImageZoomController.m
@@ -0,0 +1,65 @@
+//
+//  ImageZoomController.m
+//  SampleApp
+//
+//  Created by xiaofeng on 2022/8/20.
+//
+
+#import "ImageZoomController.h"
+
+@interface ImageZoomController () <UIScrollViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@end
+
+@implementation ImageZoomController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    self.scrollView.delegate = self;
+    self.scrollView.maximumZoomScale = 2;
+    self.scrollView.minimumZoomScale = 0.05;
+    self.scrollView.contentSize = self.imageView.frame.size;
+    
+//    self.scrollView.zoomScale = 0.1;
+    [self.scrollView setZoomScale:0.1 animated:YES];
+}
+
+#pragma mark - UIScrollViewDelegate
+
+- (UIView *) viewForZoomingInScrollView:(UIScrollView *)scrollView {
+    return self.imageView;
+}
+
+- (void) scrollViewWillBeginZooming:(UIScrollView *)scrollView withView:(UIView *)view {
+    NSLog(@"scrollViewWillBeginZooming");
+}
+- (void) scrollViewDidEndZooming:(UIScrollView *)scrollView withView:(UIView *)view atScale:(CGFloat)scale {
+    NSLog(@"scrollViewDidEndZooming");
+}
+- (void) scrollViewDidZoom:(UIScrollView *)scrollView {
+    NSLog(@"scrollViewDidZoom");
+}
+
+- (void) scrollViewWillBeginDragging:(UIScrollView *)scrollView {
+    NSLog(@"scrollViewWillBeginDragging");
+}
+- (void) scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate {
+    NSLog(@"scrollViewDidEndDragging");
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
