459d5d6954ceba14d225406df26fe14a5a9f4ba2
iaosee <iaosee@outlook.com>
2022-08-30 00:39:14 +0800

    fix: fix picker view

--- SampleApp/Picker/PickerViewController.m
+++ SampleApp/Picker/PickerViewController.m
@@ -11,17 +11,17 @@
 
 @interface PickerViewController () <UIPickerViewDataSource, UIPickerViewDelegate>
 
-@property (weak, nonatomic) NSArray<DataModel *> *list;
+@property (strong, nonatomic) NSArray<DataModel *> *list;
 @property (weak, nonatomic) IBOutlet UIPickerView *picker;
 
 @end
 
 @implementation PickerViewController
 
-- (NSArray *)list {
+- (NSArray<DataModel *> *)list {
     if (_list == nil) {
         NSArray *arrDict = [NSArray arrayWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"mockdata.plist" ofType:nil]];
-        NSMutableArray<DataModel *> *arrModels = [NSMutableArray array];
+        NSMutableArray *arrModels = [NSMutableArray array];
         for (NSDictionary *dict in arrDict) {
             DataModel *data = [[DataModel alloc] initWithDict:dict];
             [arrModels addObject:data];
@@ -34,7 +34,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    
+//    NSLog(@"%@", self.list);
     self.picker.dataSource = self;
     self.picker.delegate = self;
 }
@@ -48,12 +48,12 @@
     return self.list.count;
 }
 
-//- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component {
-//    return self.list[row].name;
-//}
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component {
+    return self.list[row].name;
+}
 
 - (UIView *)pickerView:(UIPickerView *)pickerView viewForRow:(NSInteger)row forComponent:(NSInteger)component reusingView:(UIView *)view {
-    
+
     PickerItemView *itemView = [PickerItemView pickerItem];
     itemView.frame = CGRectMake(0, 0, self.view.bounds.size.width, 80);
     itemView.data = self.list[row];
