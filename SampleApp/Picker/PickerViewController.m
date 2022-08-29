b23d7ecc484abad207a80c241c13f46f4a71a7c9
iaosee <iaosee@outlook.com>
2022-08-30 01:17:56 +0800

    feat: add datePicker component

--- SampleApp/Picker/PickerViewController.m
+++ SampleApp/Picker/PickerViewController.m
@@ -13,6 +13,10 @@
 
 @property (strong, nonatomic) NSArray<DataModel *> *list;
 @property (weak, nonatomic) IBOutlet UIPickerView *picker;
+@property (weak, nonatomic) IBOutlet UITextField *dateTextField;
+
+@property (nonatomic, strong) UIDatePicker *datePicker;
+@property (nonatomic, strong) UIToolbar *toolBar;
 
 @end
 
@@ -31,14 +35,56 @@
     return _list;
 }
 
+- (UIDatePicker *)datePicker {
+    if (_datePicker == nil) {
+        _datePicker = [[UIDatePicker alloc] init];
+        _datePicker.datePickerMode = UIDatePickerModeDate; // UIDatePickerModeDateAndTime
+        _datePicker.preferredDatePickerStyle = UIDatePickerStyleWheels; // UIDatePickerStyleInline;
+        _datePicker.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"zh-Hans"];
+    }
+    return _datePicker;
+}
+
+- (UIToolbar *)toolBar {
+    if (_toolBar == nil) {
+        _toolBar = [[UIToolbar alloc] init];
+        _toolBar.bounds = CGRectMake(0, 0, 0, 44);
+        
+        UIBarButtonItem *cancelButton = [[UIBarButtonItem alloc] initWithTitle:@"取消" style:UIBarButtonItemStylePlain target:self action:@selector(cancelHandler)];
+        UIBarButtonItem *flexSpace = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
+        UIBarButtonItem *doneButton = [[UIBarButtonItem alloc] initWithTitle:@"完成" style:UIBarButtonItemStylePlain target:self action:@selector(doneHandler)];
+        
+        _toolBar.items = @[cancelButton, flexSpace, doneButton];
+    }
+    return _toolBar;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 //    NSLog(@"%@", self.list);
     self.picker.dataSource = self;
     self.picker.delegate = self;
+    
+    self.dateTextField.inputView = self.datePicker;
+    self.dateTextField.inputAccessoryView = self.toolBar;
 }
 
+- (void) cancelHandler {
+    [self.view endEditing:YES];
+}
+- (void) doneHandler {
+    NSDate *date = self.datePicker.date;
+    //    NSString *strDate = [NSDateFormatter localizedStringFromDate:date dateStyle:NSDateFormatterShortStyle timeStyle:NSDateFormatterShortStyle];
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    formatter.dateFormat = @"yyyy年MM月dd日 HH:mm:ss";
+    NSString *strDate = [formatter stringFromDate:date];
+
+    self.dateTextField.text = strDate;
+    [self.view endEditing:YES];
+}
+
+#pragma mark - UIPickerViewDataSource, UIPickerViewDelegate
 
 - (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
     return 1;
