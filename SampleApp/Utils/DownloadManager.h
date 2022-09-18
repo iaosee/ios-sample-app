611b207bb461dcf0bf7317c2ab26afca964f5439
iaosee <iaosee@outlook.com>
2022-09-18 21:24:47 +0800

    feat: add DownloadManager

---
+++ SampleApp/Utils/DownloadManager.h
@@ -0,0 +1,16 @@
+//
+//  DownloadManager.h
+//  SampleApp
+//
+//  Created by xiaofeng on 2022/9/17.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DownloadManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SampleApp/Utils/DownloadManager.m
@@ -0,0 +1,14 @@
+//
+//  DownloadManager.m
+//  SampleApp
+//
+//  Created by xiaofeng on 2022/9/17.
+//
+
+#import "DownloadManager.h"
+
+@implementation DownloadManager
+
+
+
+@end
